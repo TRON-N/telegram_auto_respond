1a3ebce41171bb3b20a5645ea4bdb166a45d1337
Leonard <leonardvg@me.com>
2023-08-19 11:41:09 +0200

    Got the notifier to respond to forward messages from chats that I have access to, to a group that I have access to.

--- NotifierBot.py
+++ NotifierBot.py
@@ -2,21 +2,74 @@ from pyrogram import Client, filters
 from pyrogram.types import Message, BotCommand
 from pyrogram.handlers import MessageHandler
 
+class KeywordChatMatrix:
+    def __init__(self):
+        self._chat_id_keyword_dict: dict[int: set[str]] = {}
+        self._keyword_chat_id_dict: dict[str: set[int]] = {}
+
+    def _does_dict_key_keyword_exist(self, keyword: str) -> bool:
+        return keyword in self._keyword_chat_id_dict.keys()
+    
+    def _does_dict_key_chat_id_exist(self, chat_id: int) -> bool:
+        return chat_id in self._chat_id_keyword_dict.keys()
+
+    def add_keywords(self, keyword_list: list[str], chat_id: int):
+        if not self._does_dict_key_chat_id_exist(chat_id):
+            self._chat_id_keyword_dict[chat_id] = set()
+        for keyword in keyword_list:
+            normalized_keyword = keyword.lower()
+            self._chat_id_keyword_dict[chat_id].add(normalized_keyword)
+
+            if not self._does_dict_key_keyword_exist(normalized_keyword):
+                self._keyword_chat_id_dict[normalized_keyword] = set()
+            self._keyword_chat_id_dict[normalized_keyword].add(chat_id)
+
+    def try_remove_keyword(self, keyword: list[str], chat_id: int) -> bool:
+        if self._does_dict_key_chat_id_exist(chat_id):
+            try:
+                self._chat_id_keyword_dict[chat_id].remove(keyword.lower())
+                return True
+            except:
+                return False
+        else:
+            return False
+
+
+    def find_chats_for_keywords(self, keyword_list: list[str]) -> dict[str: set[int]]:
+        result_dict = {}
+        for keyword in keyword_list:
+            normalized_keyword = keyword.lower()
+            if self._does_dict_key_keyword_exist(normalized_keyword):
+                result_dict[normalized_keyword] = self._keyword_chat_id_dict[normalized_keyword]
+        return result_dict
+
+
+    def find_keywords_for_chat(self, chat_id: int) -> set[str]:
+        if self._does_dict_key_chat_id_exist(chat_id):
+            return self._chat_id_keyword_dict[chat_id]
+        else:
+            return set()
+
+    def does_chat_have_keywords(self, chat_id: int) -> bool:
+        if self._does_dict_key_chat_id_exist(chat_id):
+            return bool(self._chat_id_keyword_dict[chat_id])
+        else:
+            return False
 
 class NotifierBot:
-    def __init__(self, api_id, api_hash, bot_token):
+    def __init__(self, api_id: str, api_hash: str, bot_token: str, keyword_chat_matrix: KeywordChatMatrix):
         self.command_dictionary: dict[str, tuple[str, function]] = {
             "set": (
                 "Sets a keyword for the bot to look out for (case insensitive).",
-                self.set_key_word,
+                self.set_keyword,
             ),
             "list": (
                 "Lists the keywords that the bot is currently watching.",
-                self.list_key_words,
+                self.list_keywords,
             ),
             "remove": (
                 "Removes a keyword so that the bot no longer watches for it.",
-                self.remove_key_word,
+                self.remove_keyword,
             ),
         }
         self.bot_app = Client(
@@ -25,14 +78,16 @@ class NotifierBot:
             api_hash=api_hash,
             bot_token=bot_token,
         )
+        self._set_command_handler()
+        self._keyword_chat_matrix = keyword_chat_matrix
 
-    def _set_commands(self):
-        self.bot_app.set_bot_commands(
-            [
-                BotCommand(key, value[0])
-                for key, value in self.command_dictionary.items()
-            ]
-        )
+    # async def set_commands(self):
+    #     await self.bot_app.set_bot_commands(
+    #         [
+    #             BotCommand(key, value[0])
+    #             for key, value in self.command_dictionary.items()
+    #         ]
+    #     )
 
     def _set_command_handler(self):
         self.bot_app.add_handler(
@@ -42,18 +97,69 @@ class NotifierBot:
             )
         )
 
-    def set_key_word(self, message: Message):
+    async def set_keyword(self, message: Message):
         print("Triggered set keyword")
-        print(message)
+        reply_message = "Incorrect usage, use '/set keyword' with one or more keywords to remove them."
+        if len(message.command) > 1:
+            self._keyword_chat_matrix.add_keywords(message.command[1:], message.chat.id)
+            reply_message = "Added the following keywords:\n{}".format(
+                "\n".join([f"-  {keyword}" for keyword in message.command[1:]])
+            )
+        await self.bot_app.send_message(
+            chat_id=message.chat.id,
+            text=reply_message,
+            reply_to_message_id=message.id
+        )
 
-    def list_key_words(self, message: Message):
+    async def list_keywords(self, message: Message):
         print("Triggered list keyword")
-        print(message)
+        reply_message = "No keywords are specified, use '/set keyword' with one or more keywords to remove them."
+        if self._keyword_chat_matrix.does_chat_have_keywords(message.chat.id):
+            keyword_list = self._keyword_chat_matrix.find_keywords_for_chat(message.chat.id)
+            keywords = "\n".join([f"-  {keyword}" for keyword in keyword_list])
+            reply_message = f"The keyword list contains the following words:\n{keywords}"
 
-    def remove_key_word(self, message: Message):
+        await self.bot_app.send_message(
+            chat_id=message.chat.id,
+            text=reply_message,
+            reply_to_message_id=message.id
+        )
+
+    async def remove_keyword(self, message: Message):
         print("Triggered remove keyword")
-        print(message)
+
+        reply_message = ""
+        if len(message.command) == 1:
+            reply_message = "Incorrect usage, use '/remove keyword' with one or more keywords to remove them."
+        elif not self._keyword_chat_matrix.does_chat_have_keywords(message.chat.id):
+            reply_message = "No keywords exist, use '/set keyword' to add them."
+        else:
+            removed_list = []
+            not_removed_list = []
+            for keyword in message.command[1:]:
+                if self._keyword_chat_matrix.try_remove_keyword(keyword, message.chat.id):
+                    removed_list.append(keyword)
+                else:
+                    not_removed_list.append(keyword)
+            if removed_list:
+                reply_message = "Removed the following keywords:\n{}".format(
+                    "\n".join([f"-  {keyword}" for keyword in removed_list])
+                )
+            if not_removed_list:
+                if reply_message:
+                    reply_message += "\n"
+                reply_message += (
+                    "Could not find the following keywords for removal:\n{}".format(
+                        "\n".join([f"-  {keyword}" for keyword in not_removed_list])
+                    )
+                )
+        await self.bot_app.send_message(
+            chat_id=message.chat.id,
+            text=reply_message,
+            reply_to_message_id=message.id
+        )
 
     async def _command_handler(self, client: Client, message: Message):
         for command in message.command:
-            self.command_dictionary[command][1](message)
+            if command in self.command_dictionary.keys():
+                await self.command_dictionary[command][1](message)
--- autoResponder.py
+++ autoResponder.py
@@ -4,7 +4,7 @@ from pyrogram import Client, compose
 from pyrogram.types import Message
 from pyrogram.handlers import MessageHandler
 
-from NotifierBot import NotifierBot
+from NotifierBot import KeywordChatMatrix, NotifierBot
 
 
 async def main():
@@ -15,7 +15,8 @@ async def main():
 
     bot_token = os.getenv("TELEGRAM_BOT_API_TOKEN")
 
-    notifier_bot = NotifierBot(api_id, api_hash, bot_token)
+    keyword_chat_matrix = KeywordChatMatrix()
+    notifier_bot = NotifierBot(api_id, api_hash, bot_token, keyword_chat_matrix)
     
     church_group_id = int(os.getenv('GROUP_CHAT_ID'))
     notification_group_id = int(os.getenv('NOTIFICATION_GROUP_ID'))
@@ -24,14 +25,21 @@ async def main():
     async def my_handler(client, message: Message):
         print(f"Message text: '{message.text}'")
         print(f"Chat ID: '{message.chat.id}' Chat Title: '{message.chat.title}'")
-        if isinstance(message.text, str) and message.chat.id == church_group_id:
-            if message.text.lower().find("online") != -1:
-                await notifier_bot.bot_app.send_message(
-                    chat_id=notification_group_id,
-                    text=f"Quick, {message.from_user.username} might need an online group for someone.",
-                )
-                # await message.forward('me')
-                print(f'Sent Message to chat: {notification_group_id}')
+        if isinstance(message.text, str): #and message.chat.id == church_group_id
+            message_word_list = message.text.split()
+            keyword_chat_dict = keyword_chat_matrix.find_chats_for_keywords(message_word_list)
+            print(f"Message matched {len(keyword_chat_dict)} keywords")
+            forward_dest_list = []
+            for matched_keyword, chat_id_set in keyword_chat_dict.items():
+                for chat_id in chat_id_set:
+                    if chat_id not in forward_dest_list and message.chat.id != chat_id:
+                        print(f"Forwarding message {message.id} to chat {chat_id}")
+                        await user_app.forward_messages(
+                            chat_id=chat_id,
+                            from_chat_id=message.chat.id,
+                            message_ids=message.id
+                        )
+                        forward_dest_list.append(chat_id)
 
     MessageHandler(my_handler)
     user_app.add_handler(MessageHandler(my_handler))
